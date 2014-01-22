a92ad2c306983b743bea2955ab188ecae71a8f0e
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2014-01-22 13:12:28 -0700

    Updated workflow to clone, add, and commit a file.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main clj-jgit-sample.core
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [fs "1.3.3"]
                  [clj-jgit "0.6.4"]])
--- src/clj_jgit_sample/core.clj
+++ src/clj_jgit_sample/core.clj
@@ -1,14 +1,58 @@
-(ns clj-jgit-sample.core)
+(ns clj-jgit-sample.core
+  (:require
+            [clj-jgit.porcelain :as porcelain]
+            [clojure.contrib.io :as io]
+            [fs.core :as fs]))
 
-(:require '[clj-jgit.porcelain :as porc])
-;(:require '[clj-jgit.porcelain])
+(def repo-hosted-loc
+  "https://github.com/camlow325/clj-jgit-sample-testrepo.git")
 
-(def my-repo
-  (porc/git-clone-full "https://github.com/camlow325/clj-git-sample.git"
-                       "local-folder/shadow-of-clj-git-sample"))
+(def repo-cloned-dir
+  "/tmp/clj-git-sample-cloned")
 
-(defn -main
-  (println "I'm in main")
-  ;(clj-jgit.porcelain/git-status my-repo)
-  (porc/git-status my-repo)
-  (println ("After git status")))
+(def repo-change-file
+  "README.md")
+
+(defn repo-clean []
+  (io/delete-file-recursively repo-cloned-dir true)
+  )
+
+(defn
+  clone-repo []
+  (:repo (clj-jgit.porcelain/git-clone-full repo-hosted-loc repo-cloned-dir)))
+
+(defn display-status [git]
+  (println "Status is: " (clj-jgit.porcelain/git-status git)))
+
+(defn update-file []
+  (spit (str repo-cloned-dir "/" repo-change-file)
+        (str "Last changed: " (.toString (java.util.Date.)))))
+
+(defn add-file [repo]
+  (clj-jgit.porcelain/git-add repo repo-change-file))
+
+(defn commit-file [repo]
+  (clj-jgit.porcelain/git-commit repo "update via lein test run"
+                                 {:name "lein tester" :email "lein.tester@bogus.com"}))
+
+(defn clone-and-modify []
+  (println "Cloning repo...")
+  (let [cloned-repo (clone-repo)]
+    (println "Repo cloned")
+    (display-status cloned-repo)
+    (println "Updating file...")
+    (update-file)
+    (println "Adding file...")
+    (add-file cloned-repo)
+    (display-status cloned-repo)
+    (println "Committing file...")
+    (commit-file cloned-repo)
+    (display-status cloned-repo)
+    ))
+
+(defn -main []
+  (println "Cleaning up to start...")
+  (repo-clean)
+  (clone-and-modify)
+  (println "Leaving main"))
+
